65f150c58b4295fb8a343763ffc2555d96d0d946
cgiohidalgo <root@Giovanny-2.local>
2020-03-29 23:00:33 -0500

    new version travis soft

--- coordenadas.cpp
+++
@@ -1,5 +0,0 @@
-#include "coordenadas.h"
-
-Coordenadas::Coordenadas(){
-  
-}
--- coordenadas.h
+++
@@ -1,9 +0,0 @@
-#ifndef COORDENADAS_H
-#define COORDENADAS_H
-
-class Coordenadas{
-  public:
-  Coordenadas();
-};
-
-#endif
